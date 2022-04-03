e9fb7111a08dd957e913b268fc1b6cea40c596d7
gianni <gianni.crivello96@gmail.com>
2022-04-03 02:47:12 -0700

    first commit

--- MyTableViewController/ViewController.swift
+++ MyTableViewController/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func didTapButton1() {
+        let vc = MyTableViewController()
+        vc.models = [
+            ("apple", {print("i am a apple")}),
+            ("orange", {print("i am a orange")}),
+            ("lemon", {print("i am a lemon")})
+        ]
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    @IBAction func didTapButton2() {
+        let vc = MyTableViewController()
+        vc.models = [
+            ("Honda", {print("i am a honda")}),
+            ("Nissan", {print("i am a nissan")}),
+            ("altima", {print("i am a ho")})
+
+        ]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 
 }
