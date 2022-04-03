f39a2098d0a6c5d2eab0790015e12cb5628690c2
gianni <gianni.crivello96@gmail.com>
2022-04-03 02:17:33 -0700

    Initial Commit

---
+++ MyTableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTableViewController
+//
+//  Created by Gianni Crivello on 4/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
